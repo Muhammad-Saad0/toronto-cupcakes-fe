1a765d1ada990b0fcfebe8254e1b04778aa1e353
Muhammad-Saad0 <doritozz349@gmail.com>
2024-06-26 12:37:09 +0500

    latest

---
+++ app/Cancel/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React, { useState } from "react";
+
+function page() {
+  return (
+    <div className="m-auto font-averia">
+      <div className="flex flex-col items-center justify-center py-10">
+        <img src="/cross_button.png" alt="cross_button" />
+        <h4>Your Payment was not Successful.</h4>
+      </div>
+    </div>
+  );
+}
+
+export default page;
