beb4fa5a33c7522dd0a78c74f2d7a39fd55682c5
Muhammad-Saad0 <doritozz349@gmail.com>
2024-06-26 13:58:57 +0500

    latest

--- components/Shared/ScrollProgress.tsx
+++ components/Shared/ScrollProgress.tsx
@@ -8,14 +8,15 @@ const ScrollProgress = () => {
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
-    restDelta: 0.001
+    restDelta: 0.001,
   });
 
   return (
-    <motion.div
-      className="fixed top-0 left-0 right-0 h-2 bg-[#3D0C11] origin-left z-50"
-      style={{ scaleX }}
-    />
+    // <motion.div
+    //   className="fixed top-0 left-0 right-0 h-2 bg-[#3D0C11] origin-left z-50"
+    //   style={{ scaleX }}
+    // />
+    <div></div>
   );
 };
 
